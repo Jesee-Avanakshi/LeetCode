f6558af88f9d841834d4cb2d0af9a24bc57d2ff8
Jesee Avanakshi <113314073+Jesee-Avanakshi@users.noreply.github.com>
2025-08-28 10:24:59 -0500

    Time: 4 ms (79.08%) | Memory: 44.6 MB (51.23%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -10,26 +10,32 @@
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        if (lists==null || lists.length==0) return null;
-
+        if(lists==null || lists.length==0) return null;
+        
+        // Create a priorityQueue minheap
         PriorityQueue<ListNode> minheap = new PriorityQueue<>((a,b)->a.val-b.val);
 
+        //inserting all the heads of each list to this queue
         for(ListNode node:lists){
-            if(node!=null){
+            if(node!=null)
                 minheap.offer(node);
-            }
         }
-        ListNode dummy =new ListNode(0);
-        ListNode curr = dummy;
+        // while my queue is not empty, i am taking out minimum value node from minheap and adding it to result
+        
+        ListNode dummy = new ListNode(0);
+        ListNode curr =dummy;
 
         while(!minheap.isEmpty()){
             ListNode node = minheap.poll();
             curr.next = node;
-            curr =curr.next;
+            curr = curr.next;
+             // adding the next node of the head to heap again
             if(node.next!=null){
                 minheap.offer(node.next);
             }
         }
-        return dummy.next;
+
+    return dummy.next;
+       
     }
 }

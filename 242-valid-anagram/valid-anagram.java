f7e3a11c15acf6c88309ede65a68fac7a29f2d53
Jesee Avanakshi <113314073+Jesee-Avanakshi@users.noreply.github.com>
2025-09-29 11:36:17 -0500

    Time: 26 ms (5.60%) | Memory: 44.6 MB (43.43%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,31 +1,12 @@
 class Solution {
-
     public boolean isAnagram(String s, String t) {
-        if(s.length()!=t.length()) return false;
-        int[] result = new int[26];
-        
+        if(s.length() != t.length()) return false;
+        Map<Character,Integer> mapS = new HashMap<>();
+        Map<Character,Integer> mapT = new HashMap<>();
         for(int i=0;i<s.length();i++){
-            char charS = s.charAt(i);
-            char charT = t.charAt(i);
-            result[charS-'a']++;
-            result[charT-'a']--;
+            mapS.put(s.charAt(i),mapS.getOrDefault(s.charAt(i),0)+1);
+            mapT.put(t.charAt(i),mapT.getOrDefault(t.charAt(i),0)+1);
         }
-        for(int i=0;i<result.length;i++){
-            if(result[i] != 0){
-                return false;
-            }
-        }
-
-        return true;
-    }
-    // public boolean isAnagram(String s, String t) {
-    //     if(s.length()!=t.length()) return false;
-    //     Map<Character,Integer> mapS = new HashMap<>();
-    //     Map<Character,Integer> mapT = new HashMap<>();
-    //     for(int i=0;i<s.length();i++){
-    //         mapS.put(s.charAt(i),mapS.getOrDefault(s.charAt(i),0)+1);
-    //         mapT.put(t.charAt(i),mapT.getOrDefault(t.charAt(i),0)+1);
-    //     }
-    //     return mapS.equals(mapT);
-    // }
+        return mapS.equals(mapT);
+     }
 }
